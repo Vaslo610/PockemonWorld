19fc06de4a4d0a2b5c79e68cf4706b48c79234ba
Santiago <Santiago@DELL>
2015-08-24 08:16:03 -0500

    prueba1

--- src/pockemonworld/PockemonWorld.java
+++ src/pockemonworld/PockemonWorld.java
@@ -16,6 +16,8 @@ public class PockemonWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        parse esto es una prueba
     }
     
 }
